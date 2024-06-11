dc2addfa41098071a51034daeb54c8644db680c1
malrjane <yana.ml02@mail.ru>
2024-06-11 14:20:18 +0400

    initial commit

---
+++ js/classTeam.js
@@ -0,0 +1,6 @@
+export default class Team {
+  constructor(pers1, pers2) {
+    this.team1 = pers1;
+    this.team2 = pers2;
+  }
+}
---
+++ js/index.js
@@ -0,0 +1,32 @@
+import Character from "./classCharacter";
+import Team from "./classTeam";
+
+const pers1 = new Character('Лучник', 'Bowman', 80, 1, 30, 10);
+const pers2 = new Character('Маг', 'Magician', 60, 1, 50, 20);
+const pers3 = new Character('Стрелок', 'Swordsman', 50, 1, 40, 15);
+
+const team = new Team(pers1, pers2);
+team.pers3 = pers3;
+
+team[Symbol.iterator] = function() {
+  let i = 0;
+  let that = this;
+  return {
+    next: function () {
+      if (i < Object.keys(that).length) {
+        return {
+          value: that[Object.keys(that)[i++]],
+          done: false
+        }
+      } else {
+        return {
+          done: true
+        }
+      }
+    }
+  }
+}
+
+for(let key of team) {
+  console.log(key);
+}
